d612bf693064b265fcd450b8e31176d4a7efd4b6
giancarlo-zuliani <slaigox@gmail.com>
2020-11-16 14:26:27 +0100

    exercise done

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ var app = new Vue({
   el:'#root',
   data:{
     title: 'titolone',
-    img : 'img/img1.png'
+    img : 'img/img1.jpg'
   },
   methods:{
-    carousel : function(){
-      this.img ==='img/img1.png' ? this.img ='img/img2.png' : this.img ='img/img1.png';
+    slide : function(){
+      this.img === 'img/img1.jpg' ? this.img ='img/img2.jpg' : this.img ='img/img1.jpg';
     }
   }
-})
+});
